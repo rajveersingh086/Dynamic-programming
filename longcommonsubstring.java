4d45623883e25df22e9e7ab8e3acbb986561b7ae
Rajveer Singh <138320694+rajveersingh086@users.noreply.github.com>
2025-06-30 22:48:24 +0530

    Add files via upload

---
+++ longcommonsubstring.java
@@ -0,0 +1,25 @@
+package DP;
+
+public class longcommonsubstring {
+
+    public int longestCommonSubstr(String s1, String s2) {
+        // code here
+        int m = s1.length();
+        int n = s2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        int maxLength = 0;
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    maxLength = Math.max(maxLength, dp[i][j]);
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+
+        return maxLength;
+    }
+}
