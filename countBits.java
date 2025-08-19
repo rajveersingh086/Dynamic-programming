7eb8e04a4aac45ecb45c40f1e40b820c9292ca92
Rajveer Singh <138320694+rajveersingh086@users.noreply.github.com>
2025-08-19 23:08:15 +0530

    Add files via upload

---
+++ countBits.java
@@ -0,0 +1,12 @@
+package Math;
+
+class countBits {
+    public int[] countBits(int n) {
+        int []dp = new int [n+1];
+        for(int i=1;i<=n;i++){
+            dp[i] = dp[i>>1] + (i&1);
+        }
+         return dp;
+    }
+   
+}
